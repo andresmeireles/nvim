867d500b3599f587bd2000ad4a59e8ce0e7825c4
André Meireles <andre2meireles@gmail.com>
2021-11-21 13:53:38 -0300

    phpstan linter config

--- lua/plugins_config/ale_linters.lua
+++ lua/plugins_config/ale_linters.lua
@@ -3,7 +3,13 @@ local linters = {}
 local igored_linters = {}
 
 igored_linters = {'cspell'}
-linters["php"] = {"phpstan"}
+linters["php"] = {"phpstan", "psalm"}
+
+-- PHP LINTERS
+-- PHPSTAN
+g.ale_php_phpstan_executable = "./vendor/bin/phpstan"
+g.ale_php_phpstan_configuration = "./phpstan.neon"
+g.ale_php_phpstan_memory_limit = "512M"
 
 g.ale_linters = linters
 g.ale_linters_ignore = igored_linters

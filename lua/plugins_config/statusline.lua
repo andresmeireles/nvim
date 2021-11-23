c6ddffe3b61b13650b5ac0fdf98c916be0bc50ba
André Meireles <andre2meireles@gmail.com>
2021-11-23 17:38:54 -0300

    status line

--- lua/plugins_config/statusline.lua
+++ lua/plugins_config/statusline.lua
@@ -3,6 +3,13 @@ vim.cmd [[set noshowmode]]
 require "lualine".setup {
     sections = {
         lualine_c = {
+            {
+                'filename',
+                file_status = true, -- displays file status (readonly status, modified status)
+                path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+            }
+        },
+        lualine_d = {
         'lsp_progress'
         }
     },
